2038f12dc7ece09532dced54945cfa8fd11379b1
Udesh Liyanaarachchi <udesh.liyanaarachchi@gmail.com>
2011-04-07 07:57:00 -0700

    this is chat application build from java

---
+++ src/chat/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package chat;
+
+/**
+ *
+ * @author ATC-19
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        chat ss=new chat();
+        ss.setVisible(true);
+
+    }
+
+}
